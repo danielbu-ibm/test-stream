9280fc338cc3401107e3e45410defe869a0dc0b3
Daniel Butler <dannyeb@gmail.com>
2023-08-02 10:15:00 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-/********************************************************************
-This file is used to implement the ROOT module.
-*********************************************************************/
+##############################################################################
+# Resource Group
+##############################################################################
+module "resource_group" {
+  source = "terraform-ibm-modules/terraform-ibm-resource-group.git"
+  version = "latest"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
--- variables.tf
+++ variables.tf
@@ -2,8 +2,26 @@
 # Input Variables
 ##############################################################################
 
-#variable "my_variable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = "default_value"
-#}
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud API Key"
+  sensitive   = true
+}
+
+variable "region" {
+  description = "The region where VPC and services are deployed"
+  type        = string
+  default     = "us-south"
+}
+
+variable "prefix" {
+  description = "The prefix that you would like to append to your resources"
+  type        = string
+  default     = "vpe"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
+  default     = null
+}
