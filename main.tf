0087dc988bd23ca642aa8ce3e4d33dd167505837
Daniel Butler <dannyeb@gmail.com>
2023-08-02 13:15:26 +0100

    uppdate tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 # Resource Group
 ##############################################################################
 module "resource_group" {
-  source = "terraform-ibm-modules/terraform-ibm-resource-group.git"
-  version = "latest"
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.0.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
